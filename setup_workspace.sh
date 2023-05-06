6c368cd7577192fcb988bdd9e8da609993e5fde8
Peter Gaskell <pgaskell@umich.edu>
2023-05-05 22:56:54 -0400

    added simple workspace setup

---
+++ setup_workspace.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir ~/Mbot-Project-Development
+cd ~/Mbot-Project-Development
+git clone git@github.com:MBot-Project-Development/mbot_lcm_base.git
+git clone git@github.com:MBot-Project-Development/mbot_firmware.git
+git clone git@github.com:MBot-Project-Development/mbot_bridge.git
+git clone git@github.com:MBot-Project-Development/mbot_autonomy.git
+git clone git@github.com:MBot-Project-Development/mbot_gui.git
+git clone git@github.com:MBot-Project-Development/mbot_web_app.git
+git clone git@github.com:MBot-Project-Development/rplidar_lcm_driver.git
+git clone git@github.com:MBot-Project-Development/Documentation.git
