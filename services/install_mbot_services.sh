feac87dfa3bc3aaab65fde90e9677bd8f6bdabe1
Jana Pavlasek <pavlasek@umich.edu>
2023-06-14 10:17:12 -0400

    Rename services using hyphens, add correct restart specifier, add restart delay for LCM serial service

--- services/install_mbot_services.sh
+++ services/install_mbot_services.sh
@@ -2,17 +2,34 @@
 
 set -e  # Quit on error.
 
+SERVICE_LIST="mbot-start-network
+              mbot-publish-info
+              mbot-lcm-serial"
+
 # Copy the scripts we need for the services.
 sudo cp mbot_start_networking.py /usr/local/etc/
 sudo cp mbot_publish_info.sh /usr/local/etc/
 
 # Copy the services.
-sudo cp mbot_start_network.service /etc/systemd/system/mbot_start_network.service
-sudo cp mbot_publish_info.service /etc/systemd/system/mbot_publish_info.service
-sudo cp mbot_lcm_serial.service /etc/systemd/system/mbot_lcm_serial.service
+for serv in $SERVICE_LIST
+do
+    sudo cp $serv.service /etc/systemd/system/$serv.service
+done
 
 # Enable the services.
 sudo systemctl daemon-reload
-sudo systemctl enable mbot_start_network.service
-sudo systemctl enable mbot_publish_info.service
-sudo systemctl enable mbot_lcm_serial.service
+for serv in $SERVICE_LIST
+do
+    sudo systemctl enable $serv.service
+    # echo "Copying $serv"
+done
+
+# Success message.
+echo
+echo "Installed and enabled the following services:"
+echo
+for serv in $SERVICE_LIST
+do
+    echo "    $serv.service"
+done
+echo
