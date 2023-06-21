991adf91dc55e9aec736dcf06a462a79804e9506
Multyxu <multyxu@gmail.com>
2023-06-21 16:06:30 -0400

    fix ssh-agent issue, path issue etc.

--- comprehensive_setup.sh
+++ comprehensive_setup.sh
@@ -34,18 +34,22 @@ sudo echo 'gpu_mem=128' >> /boot/config.txt
 
 #not setting desktop colors :(
 
-cd ~/mbot_workspace
+cd ..
 # Clone packages
 echo "[SETUP] cloning repos"
 repos=("mbot_lcm_base" "mbot_firmware" "mbot_autonomy" "mbot_gui" "mbot_web_app" "rplidar_lcm_driver")
 
+eval "$(ssh-agent -s)"
+ssh-add /home/mbot/.ssh/id_ed25519
+
+# clone the repo if the folder not exists
 for repo in "${repos[@]}"; do
-    git clone "git@github.com:MBot-Project-Development/$repo.git"
+    [ ! -d "/home/mbot/mbot_workspace/$repo" ] && git clone "git@github.com:MBot-Project-Development/$repo.git"
 done
 
 # sys utils setup
 echo "[SETUP] setup for sys_utils"
-cd ~/mbot_workspace/mbot_sys_utils
+cd /home/mbot/mbot_workspace/mbot_sys_utils
 sudo ./install_scripts/install_mbot_dependencies.sh
 ./install_scripts/install_lcm.sh
 sudo ./install_scripts/install_nomachine.sh
@@ -59,9 +63,10 @@ cd ../services
 
 # Setting up other repos
 # mbot_lcm_base
-cd ~/mbot_workspace/mbot_lcm_base
+cd /home/mbot/mbot_workspace/mbot_lcm_base
 echo "[SETUP] setup for lcm_base"
 git checkout v0.1 && git pull
+[ -d "./build" ] && sudo rm -rf build
 mkdir build && cd build
 cmake ..
 make
@@ -69,9 +74,10 @@ sudo make install
 echo "[SETUP] finished setup for lcm_base"
 
 # rplidar_lcm_driver
-cd ~/mbot_workspace/rplidar_lcm_driver
+cd /home/mbot/mbot_workspace/rplidar_lcm_driver
 echo "[SETUP] setup for rplidar_lcm_driver"
 git pull && git submodule update --init --recursive
+[ -d "./build" ] && sudo rm -rf build
 mkdir build && cd build
 cmake ..
 make
@@ -81,7 +87,7 @@ cd ../services
 echo "[SETUP] finished setup for rplidar_lcm_driver"
 
 # mbot_web_app
-cd ~/mbot_workspace/mbot_web_app
+cd /home/mbot/mbot_workspace/mbot_web_app
 echo "[SETUP] setup for web_app"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 source ~/.bashrc #get nvm without launching new window by sourcing bashrc again
@@ -91,7 +97,7 @@ nvm install 18
 echo "[SETUP] finished setup for web_app"
 
 # mbot_firmware
-cd ~/mbot_workspace/mbot_firmware
+cd /home/mbot/mbot_workspace/mbot_firmware
 echo "[SETUP] setup for firmware"
 git checkout main && git pull
 sudo ./setup.sh
@@ -107,13 +113,14 @@ sudo make install
 cd ..
 
 # Build firmware, but don't upload (not sure if SWD is connected)
+[ -d "./build" ] && sudo rm -rf build
 mkdir build && cd build
 cmake ..
 make
 echo "[SETUP] finished setup for firmware"
 
 # mbot_autonomy
-cd ~/mbot_workspace/mbot_autonomy
+cd /home/mbot/mbot_workspace/mbot_autonomy
 echo "[SETUP] setup for autonomy"
 git checkout main && git pull
 sudo ./scripts/install.sh
