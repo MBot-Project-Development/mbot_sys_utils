4b95c74ef28c2eac864be072713de32482345607
Tom Gao <zimingg2001@gmail.com>
2023-08-07 10:13:18 -0400

    Moved libcamera-dev under raspi install only to avoid error on Jetson

--- install_scripts/install_mbot_dependencies.sh
+++ install_scripts/install_mbot_dependencies.sh
@@ -12,7 +12,7 @@ apt -y install build-essential wget dkms \
     autoconf automake autotools-dev gdb libglib2.0-dev libgtk2.0-dev \
     libusb-dev libusb-1.0-0-dev freeglut3-dev libboost-dev libatlas-base-dev \
     libgsl-dev libjpeg-dev default-jdk doxygen openssl libssl-dev libdc1394-dev \
-    libcamera-dev v4l-utils minicom
+    v4l-utils minicom
 
 apt -y install mesa-common-dev libgl1-mesa-dev libglu1-mesa-dev
 
@@ -24,7 +24,7 @@ apt -y install python3-dev python3-numpy python3-matplotlib python3-opencv pytho
 # Check if running on RPi
 if grep -q "Raspberry Pi" /proc/device-tree/model; then
     echo "RPi detected, installing raspi-config"
-    apt -y install raspi-config
+    apt -y install raspi-config libcamera-dev 
     raspi-config nonint do_vnc 0
     raspi-config nonint do_ssh 0
     raspi-config nonint do_camera 0
